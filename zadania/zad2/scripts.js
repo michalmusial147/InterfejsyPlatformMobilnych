6dfa635bdb65175eb82092f5d62a61858a4db2ab
mmusial <mmusial@consdata.com>
2021-12-11 11:39:05 +0100

    dwie walidacje na scieżkach  dysku

--- zadania/zad2/scripts.js
+++ zadania/zad2/scripts.js
@@ -4,19 +4,20 @@ const etPathExtended = document.getElementById('disc_path_extended');
 
 const etcPath = document.getElementById('etc_path');
 
-discPath.addEventListener('change', validate)
+// discPath.addEventListener('change', validate)
+//
+etPathExtended.addEventListener('change', validated_disc_path_extended)
+//
+// etcPath.addEventListener('change', validate)
 
-etPathExtended.addEventListener('change', validate)
-
-etcPath.addEventListener('change', validate)
-
-function validate(e) {
+function validated_disc_path_extended(e) {
+    const regex = new RegExp(
+        /[a-zA-Z]:[\\](windows|winnt|win|dos|msdos)[\\](?:[a-zA-Z0-9]+[\\])*([a-zA-Z0-9])*/, "i");
     e.preventDefault();
    const text = e.target.value;
    let validationResult = false;
     if (text.length !== 0){
-        // todo walidacje
-        validationResult = false;
+        validationResult = regex.test(text)
     }
     validationResult ? e.target.setCustomValidity('') : e.target.setCustomValidity('invalid');
 }
