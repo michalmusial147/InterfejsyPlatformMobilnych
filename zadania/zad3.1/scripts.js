d846da65d98b6f220a725168c88a34a67df903b5
mmusial <mmusial@consdata.com>
2022-01-15 12:57:48 +0100

    poprawka

--- zadania/zad3.1/scripts.js
+++ zadania/zad3.1/scripts.js
@@ -1,5 +1,16 @@
 let xHist = 0;
 let yHist = 0;
+const centerRectWidth = 50;
+const centerRectHeight = 50;
+
+
+
+function intersectRect(r1, r2) {
+    return !(r2.left > r1.right ||
+        r2.right < r1.left ||
+        r2.top > r1.bottom ||
+        r2.bottom < r1.top);
+}
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -32,12 +43,18 @@ function dragElement(elmnt) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         // set the element's new position:
-        console.log(xHist);
-        console.log(yHist);
+        // console.log(xHist);
+        // console.log(yHist);
+        const object = elements.find(el => el.id === e.target.id)
+        console.log(object);
+        const doOverlap = false;
+
         if(elmnt.offsetTop - pos2 > 270) {
             elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            object.top = (elmnt.offsetTop - pos2);
         }
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        object.left = (elmnt.offsetLeft - pos1);
     }
 
     function closeDragElement() {
@@ -59,13 +76,18 @@ function generateBox() {
     currentColor = randomColor;
     console.log(randomColor);
     para.innerHTML = `
-    <div id="mydiv${elements.length}" draggable="true" style="top:270px; height: 100px; width: 100px; background: #${randomColor}; position:absolute;">
+    <div id="element${elements.length}" draggable="true" style="top:270px; height: 100px; width: 100px; background: #${randomColor}; position:absolute;">
      <div id="mydivheader"></div>
+   ${elements.length}
     </div>
     `;
     generatedBox.appendChild(para);
-    dragElement(document.getElementById(`mydiv${elements.length}`));
-    elements.push(elements.length);
+    dragElement(document.getElementById(`element${elements.length}`));
+    elements.push({
+        id: `element${elements.length}`,
+        top: 0,
+        left: 0
+    });
 
 }
 generate.addEventListener('click', generateBox)
